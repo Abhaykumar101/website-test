706a961de526b2121dd050452c80a8d9fa96a204
abhay kumar <abhay.k@digitalumbrel.la>
2023-12-26 13:05:31 +0530

    28th

--- component/section2/modal.tsx
+++ component/section2/modal.tsx
@@ -42,6 +42,54 @@ export default function Modal() {
           </div>
         )}
       </div>
+
+      <style>
+        {
+          `
+          body.active-modal {
+            overflow-y: hidden;
+            
+        }
+        .modal{
+            position: relative;
+            z-index: 99999;
+            display: block;
+        
+        }
+        
+        .modal, .overlay {
+            width: 100vw;
+            height: 100vh;
+            top: 0;
+            left: 0;
+            right: 0;
+            bottom: 0;
+            position: fixed;
+        }
+        
+        .overlay {
+            background: rgba(49,49,49,0.8);
+        }
+        .modal-content {
+            position: absolute;
+            top: 40%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+            background: none;
+            padding: 14px 28px;
+            border-radius: 3px;
+        
+        }
+        
+        .close-modal {
+            position: absolute;
+            top: 10px;
+            right: 10px;
+            padding: 5px 7px;
+        }
+          `
+        }
+      </style>
     </>
   );
 }
