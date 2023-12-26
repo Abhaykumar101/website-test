1bb7483ee7a29dce6aae1bae529eea8c4506d6a9
abhay kumar <abhay.k@digitalumbrel.la>
2023-12-26 13:02:06 +0530

    27th

--- component/section2/modal.tsx
+++ component/section2/modal.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React, { useState } from "react";
-import "./Modal.css";
+import "./modal.css";
 
 export default function Modal() {
   const [modal, setModal] = useState(false);
